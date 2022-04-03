ecd0015b3882a630809e48545682b650da5493e6
sanket-walanj <code.sanky@gmail.com>
2022-04-03 16:24:24 +0530

    Final

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-const modal = document.querySelector('.modal'),
-  btn = document.querySelector('.btn'),
-  close = document.querySelector('.close');
+const btn = document.querySelector('.btn');
+const result = document.querySelector('.result');
 
-btn.addEventListener('click', openModal);
-close.addEventListener('click', closeModal);
-modal.addEventListener('click', closeModal);
+btn.addEventListener('click', palindrome);
 
-// open Modal
-function openModal(e) {
-  e.preventDefault();
-  modal.style.display = 'block';
-}
-// close Modal
-function closeModal(e) {
-  modal.style.display = 'none';
+function palindrome() {
+  const word = document.querySelector('.input-text').value;
+  let len = word.length;
+
+  let start = word.substring(0, Math.floor(len / 2)).toLowerCase();
+  // alert(start);
+  let end = word.substring(len - Math.floor(len / 2)).toLowerCase();
+  // alert(end);
+  // let flip = end.split('').reverse().join('');
+  let flip = [...end].reverse().join('');
+  // alert(flip);
+  if (start === flip) {
+    result.innerHTML = `${word.toUpperCase()} is a Palindrome`;
+  } else {
+    result.innerHTML = `${word.toUpperCase()} is not a Palindrome`;
+  }
 }
